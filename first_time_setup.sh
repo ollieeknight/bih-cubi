83b42aac837a3731d9dfb6a8999e04ea43352c5b
Oliver Knight <oliver.c.knight@gmail.com>
2024-11-03 18:07:12 +0100

    Updated default environments

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -2,132 +2,130 @@
 
 cd $HOME
 
-bin_folder=${HOME}/work/bin
-mkdir -p ${bin_folder}
+bin_folder="${HOME}/work/bin"
+mkdir -p "${bin_folder}"
 
-echo -e "\033[0;33mINPUT REQUIRED:\033[0m Do you want to create easy access shortcuts for folders? (y/n)" && read -r choice
+prompt_user() {
+    local prompt_message="$1"
+    local user_choice
+    echo -ne "\033[0;33mINPUT REQUIRED:\033[0m ${prompt_message} (y/n): " > /dev/tty
+    read -rn 1 user_choice < /dev/tty  # -n 1 reads only one character without a newline
+    while [[ ! $user_choice =~ ^[YyNn]$ ]]; do
+        echo -e "\n\033[0;31mERROR:\033[0m Invalid input; please enter y or n" > /dev/tty
+        echo -ne "\033[0;33mINPUT REQUIRED:\033[0m ${prompt_message} (y/n): " > /dev/tty
+        read -rn 1 user_choice < /dev/tty
+    done
+    printf '%s' "$user_choice"
+}
 
-while [[ ! $choice =~ ^[YyNn]$ ]]; do
-    echo -e "\033[0;31mERROR:\033[0m Invalid input; please enter y or n" && read -r choice
-done
+manage_symlink() {
+    local link_name="$1"
+    local home_link="$HOME/$link_name"
+    local bin_link="$bin_folder/$link_name"
 
-# Process choices
-if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
-    ln -s /data/cephfs-2/unmirrored/projects/romagnani-share ${HOME}/share
-    ln -s /fast/groups/ag_romagnani ${HOME}/group
-    mkdir -p ${HOME}/.config && mv .config ${bin_folder} && ln -s ${bin_folder}/.config ${HOME}/.config
-    mkdir -p ${HOME}/.cache && mv .cache ${bin_folder} && ln -s ${bin_folder}/.cache ${HOME}/.cache
-    mkdir -p ${HOME}/.local && mv .local ${bin_folder} && ln -s ${bin_folder}/.local ${HOME}/.local
-    mkdir -p ${HOME}/ondemand && mv ondemand ${bin_folder} && ln -s ${bin_folder}/ondemand ${HOME}/ondemand
-fi
+    mkdir -p "$bin_folder/${link_name}"
 
-echo -e "\033[0;33mINPUT REQUIRED:\033[0m Do you want to install miniconda3? (y/n)" && read -r choice
+    if [ -L "$home_link" ]; then
+        [ "$(readlink "$home_link")" != "$bin_link" ] && rm "$home_link" && ln -s "$bin_link" "$home_link"
+    elif [ -d "$home_link" ]; then
+        [ "$home_link" != "$bin_link" ] && mv "$home_link" "$bin_folder" && ln -s "$bin_link" "$home_link"
+    elif [ ! -e "$home_link" ]; then
+        mkdir -p "$bin_link" && ln -s "$bin_link" "$home_link"
+    else
+        echo -e "\033[0;31mERROR:\033[0m $home_link is a file or another type. Skipping." > /dev/tty && exit 1
+    fi
+}
 
-while [[ ! $choice =~ ^[YyNn]$ ]]; do
-    echo -e "\033[0;31mERROR:\033[0m Invalid input; please enter y or n" && read -r choice
-done
+create_symlinks() {
+    local links=(".config" ".celltypist" ".gsutil" ".ipython" ".java" ".jupyter" ".keras" ".local" ".ncbi" ".nv" ".nextflow" "ondemand" ".parallel")
+    for link in "${links[@]}"; do
+        manage_symlink "$link"
+    done
 
-# Process choices
-if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
+    ln -sf /data/cephfs-2/unmirrored/projects/romagnani-share share
+    ln -sf /data/cephfs-2/unmirrored/groups/romagnani group
+    echo "" >> "${HOME}/.bashrc"
+    echo "mkdir -p ~/scratch/tmp/.cache" >> "${HOME}/.bashrc"
+}
+
+install_miniforge() {
+    [ -d "${bin_folder}/miniforge3/" ] && rm -rf "${bin_folder}/miniforge3/"
+    [ -d "${HOME}/.conda" ] || [ -L "${HOME}/.conda" ] && rm -rf "${HOME}/.conda"
 
     cd ${bin_folder}
-    curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p ${bin_folder}/miniconda3
-    rm Miniconda3-latest-Linux-x86_64.sh
+    curl -L https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -o Miniforge3-Linux-x86_64.sh > /dev/null 2>&1
+    bash Miniforge3-Linux-x86_64.sh -b -p ${bin_folder}/miniforge3/ > /dev/null
+    rm Miniforge3-Linux-x86_64.sh
 
-    echo 'channels:
-    - conda-forge
-    - bioconda
-    - bih-cubi
-    - defaults
+    cat <<EOF > ${HOME}/.condarc
+channels:
+  - https://prefix.dev/conda-forge
+  - https://prefix.dev/pytorch
+  - https://prefix.dev/bioconda
 show_channel_urls: true
 changeps1: true
-channel_priority: flexible' > ${HOME}/.condarc
+channel_priority: flexible
+EOF
 
-    source ${bin_folder}/miniconda3/etc/profile.d/conda.sh
-    conda upgrade --all -y
-    conda config --set solver libmamba
+    source ${bin_folder}/miniforge3/etc/profile.d/conda.sh
+    echo "" >> "${HOME}/.bashrc"
+    sed -i '/conda activate/d; /conda source/d; /source .*\.conda\.sh/d' "${HOME}/.bashrc"
+    echo "source ${bin_folder}/miniforge3/etc/profile.d/conda.sh" >> "${HOME}/.bashrc"
 
-fi
+    [ ! -f "${HOME}/.Rprofile" ] || ! grep -q "options(download.file.method = 'wget')" "${HOME}/.Rprofile" && echo "options(download.file.method = 'wget')" >> "${HOME}/.Rprofile"
 
-echo -e "\033[0;33mINPUT REQUIRED:\033[0m Do you want to create R_4.3.3? (y/newname/n)" && read -r choice
+    conda upgrade --all -y > /dev/null
 
-# Validate user input
-while [[ ! $choice =~ ^[YyNn]$ && $choice != "newname" ]]; do
-    echo -e "\033[0;31mERROR:\033[0m Invalid input. Please enter Y, N, or newname" && read -r choice
-done
+    mv "${HOME}/.conda" "${bin_folder}" && ln -sf "${bin_folder}/.conda" "${HOME}/.conda"
 
-environment=$HOME/group/work/bin/source/R_4.3.3.yml
-# Process user choice
-if [[ $choice =~ ^[Yy]$ ]]; then
-    echo ""
-    echo "Creating a new environment for RStudio. This might take a while..."
-    echo ""
-    env_name='R_4.3.3'
-    conda env create -f $environment
-elif [[ $choice == "newname" ]]; then
-    echo ""
-    echo "Enter the name for the new environment:"
-    read -r env_name
+    [ -d ".cache" ] && mv .cache ~/scratch/tmp/ && ln -sf ~/scratch/tmp/.cache ~/.cache
+    [ -L ".cache" ] && rm .cache && ln -sf ~/scratch/tmp/.cache ~/.cache
+}
 
-    cp "$environment" "${TMPDIR}/${env_name}.yml"
+create_rstudio_env() {
+    local env_file="$HOME/group/work/bin/source/R_4.3.3.yml"
+    local env_name="R_4.3.3"
 
-    echo ""
-    echo "Creating a new environment named '${env_name}' for RStudio..."
-    echo ""
-
-    sed -i "1s/.*/name: ${env_name}/" "${TMPDIR}/${env_name}.yml"
-
-    conda env create -f "${TMPDIR}/${env_name}.yml"
-
-    echo "New environment '${env_name}' created."
-
-elif [[ $choice =~ ^[Nn]$ ]]; then
-    echo "Not creating a new environment for RStudio."
-fi
-
-echo -e "\033[0;33mINPUT REQUIRED:\033[0m Do you want to create a reticulate environment for R? (y/n)" && read -r choice
-
-# Validate user input
-while [[ ! $choice =~ ^[YyNn] ]]; do
-    echo -e "\033[0;31mERROR:\033[0m Invalid input. Please enter y or n" && read -r choice
-done
-
-# Process user choice
-if [[ $choice =~ ^[Yy]$ ]]; then
-    echo ""
-    echo "Creating r-reticulate, this might take a while..."
-    echo ""
-    conda env create -f $HOME/group/work/bin/source/r-reticulate.yml
-elif [[ $choice =~ ^[Nn]$ ]]; then
-    echo "Not creating r-reticulate"
-fi
-
-if [[ -d ${HOME}/ondemand/dev ]]; then
-    rm -r ${HOME}/ondemand/dev
-fi
-
-echo -e "\033[0;33mINPUT REQUIRED:\033[0m Do you want to correct OnDemand for RStudio? (y/n)" && read -r choice
-
-while [[ ! $choice =~ ^[YyNn]$ ]]; do
-    echo -e "\033[0;31mERROR:\033[0m Invalid input. Please enter y or n"
-    read -r choice
-done
-
-# Process choices
-if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
-    mkdir -p ${HOME}/ondemand/dev
-    cd ${HOME}/ondemand/dev
-    git clone https://github.com/ollieeknight/ondemand-rstudio-server
-    source $HOME/work/bin/miniconda3/etc/profile.d/conda.sh
-    conda activate
-    miniconda_dir=$(echo $PATH | grep -o "$HOME/[^:]*miniconda3" | head -n 1)
-    line_number=$(grep -n 'LD_LIBRARY_PATH=/usr/lib64/' ${HOME}/ondemand/dev/ondemand-rstudio-server/template/script.sh.erb | cut -d ":" -f 1)
-    if [[ -n $line_number ]]; then
-        sed -i "${line_number}a   export LD_PRELOAD=${miniconda_dir}/envs/$env_name/lib/libstdc++.so.6" ${HOME}/ondemand/dev/ondemand-rstudio-server/template/script.sh.erb
-    else
-    echo -e "\033[0;31mERROR:\033[0m LD_LIBRARY_PATH line not found in script.sh"
+    if [[ $1 == "newname" ]]; then
+        echo "Enter the name for the new environment:" > /dev/tty && read -r env_name < /dev/tty
+        cp "$env_file" "${TMPDIR}/${env_name}.yml"
+        sed -i "1s/.*/name: ${env_name}/" "${TMPDIR}/${env_name}.yml"
+        env_file="${TMPDIR}/${env_name}.yml"
     fi
-else
-   echo "Exiting"
+
+    conda env create -f "$env_file" > /dev/null
+    ln -s ${bin_folder}/miniforge3/envs/${env_name}/lib/R/library/ R
+}
+
+create_reticulate_env() {
+    conda env create -f $HOME/group/work/bin/source/r-reticulate.yml > /dev/null
+}
+
+# Modified function calls to properly capture the output
+choice=$(prompt_user "Do you want to create easy access shortcuts for folders?")
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+    echo "Creating symlinks..." > /dev/tty
+    create_symlinks
 fi
+
+choice=$(prompt_user "Do you want to install Miniforge3?")
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+    echo "Installing Miniforge3..." > /dev/tty
+    install_miniforge
+fi
+
+choice=$(prompt_user "Do you want to create RStudio environment R_4.3.3? (y/newname/n)")
+if [[ "$choice" =~ ^[Yy]$ || "$choice" == "newname" ]]; then
+    echo "Creating RStudio environment..." > /dev/tty
+    create_rstudio_env "$choice"
+fi
+
+choice=$(prompt_user "Do you want to create a reticulate environment for R?")
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+    echo "Creating reticulate environment..." > /dev/tty
+    create_reticulate_env
+fi
+
+conda activate
+conda clean --all -y
+pip cache purge
